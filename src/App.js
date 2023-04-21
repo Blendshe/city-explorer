153fd0298604774445e0787914c50ff9983aed92
JaneG <getwize@googlemail.com>
2023-04-21 18:59:23 +0100

    loads

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [form, setForm] = useState({ city: "" });
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState({});
+
+  async function getLocation() {
+    const API =
+      // insert API key here
+      "";
+    const res = await axios.get(API);
+    setLocation(res.data[0]);
+  }
+
+  function handleType(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <h1>City Form</h1>
+      <p>City: {form.city}</p>
+      <form onClick={handleSubmit}>
+        {/* <label htmlFor="city">City</label> */}
+        <input
+          name="city"
+          placeholder="Name your City"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Explore!" />
+      </form>
+      {/* this all needs sorting in realtion to the form functions handleType */}{" "}
+      */}
+      {/* <div className="App"> */}
+      {/* <input onChange={handleType} /> */}
+      {/* <button onClick={getLocation}>Get Location Details</button> */}
+      {/* <h1>The city is: {location.display_name}</h1> */}
+    </div>
+  );
 }
 
 export default App;
